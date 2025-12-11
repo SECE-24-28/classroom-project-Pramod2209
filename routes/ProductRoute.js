1a6e5c8f81866ddbe35a42f8ab2d2e661ef191db
Pramod <kpramodkumar2006@gmail.com>
2025-12-11 21:58:26 +0530

    Delete routes directory

--- routes/ProductRoute.js
+++
@@ -1,7 +0,0 @@
-const express =require("express");
-const { getProduct } = require("../controller/ProductController");
-const router=express.Router();
-
-router.get("/sleepingproduct",getProduct)
-
-module.exports=router;
