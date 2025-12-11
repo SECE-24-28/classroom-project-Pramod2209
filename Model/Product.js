79233d8196fbcd6f2636da44f32e15c80b65756c
Pramod <kpramodkumar2006@gmail.com>
2025-12-11 21:57:41 +0530

    Delete Model directory

--- Model/Product.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-
-const productSchema=mongoose.Schema({
-    name:{type: String,required:true},
-    description:{type: String, required: true}
-})
-
-module.exports=mongoose.model("Product",productSchema);
